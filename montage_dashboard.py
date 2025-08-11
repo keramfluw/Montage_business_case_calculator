d6483133d03865e809020a4b31a775084b63a98f
keramfluw <marek.wulff@t-online.de>
2025-08-11 13:41:49 +0200

    Update montage_dashboard.py

--- montage_dashboard.py
+++ montage_dashboard.py
@@ -20,8 +20,7 @@ montage_daten = {
     "Rauchmelder Neuausstattung und Austausch": (0.25, 8.00)
 }
 
-st.title("beatz&wulff services GmbH
-Wirtschaftlichkeits-Dashboard für Montagen")
+st.title("beatz&wulff services GmbH Wirtschaftlichkeits-Dashboard für Montagen")
 
 # Eingabe: Stundensatz und Anzahl Monteure
 st.sidebar.header("Allgemeine Einstellungen")
